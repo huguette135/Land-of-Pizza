caa1460cba362b35c1dc09c0269b4391fc4ec705
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-10-20 11:52:12 +0200

    added js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
+$(document).ready(function(){
+    $('#checkout').click(function() {
 
+        var pizza = $("#size").val();
+        var number = $("#number").val();
+
+        var totalAmount = pizza * number;
+    
+        $("#output").text("here is the total amount  "+ totalAmount);
+       
+        
+
+    })
+})
